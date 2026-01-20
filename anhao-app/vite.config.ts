ce8e68b2ff6ec445dee2a075c8afabc84c66a618
joy71611009 <162026102+joy71611009@users.noreply.github.com>
2026-01-20 18:22:35 +0800

    Update vite.config.ts

--- anhao-app/vite.config.ts
+++ anhao-app/vite.config.ts
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -6,8 +5,9 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
+    sourcemap: false
   },
-  server: {
-    port: 3000,
+  resolve: {
+    extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
   }
 });
